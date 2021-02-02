6b28ab1c88dd36ec0eddf4a3d63f9706b2d13c3e
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2021-02-02 20:51:39 +0100

    Done medias and create expandable btn

--- js/main.js
+++ js/main.js
@@ -5,17 +5,35 @@ burger.addEventListener('click', ()=>{
     menu.classList.toggle('active')
     if(menu.classList.contains('active')){
         menu.style.opacity = 1
-        document.body.style.overflow= 'hidden';
     } else{
         menu.style.opacity = 0
-        document.body.style.overflowY = 'initial'
     }
 })
+
+const whyUsSection = document.querySelector('.items-awards')
+const whyUsItem = document.querySelector('.items-awards__item')
+const moreBtn = document.querySelector('.more-items')
 
 window.addEventListener('resize', ()=>{
     if(window.innerWidth > 854){
         menu.style.opacity = 1
         menu.classList.remove('active')
-    } 
+    }  
 })
 
+const mediaQuery = window.matchMedia('(max-width: 768px)')
+if(mediaQuery.matches){
+    whyUsSection.style.height = `${whyUsItem.clientHeight}px`
+    moreBtn.style.display= 'block'
+} else {
+    moreBtn.style.display= 'none'
+    whyUsSection.style.height = 'auto'
+}
+
+moreBtn.addEventListener('click', ()=>{
+    if(whyUsSection.style.height === `${whyUsItem.clientHeight}px`){
+        whyUsSection.style.height = 'auto'
+    } else {
+        whyUsSection.style.height = `${whyUsItem.clientHeight}px`
+    }
+})
